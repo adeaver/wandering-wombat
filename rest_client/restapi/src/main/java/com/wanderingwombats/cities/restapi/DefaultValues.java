8c091d03bdbb9a87067a7a78a874eb7b6c3abf49
adeaver <deaver.andrew@gmail.com>
2016-01-23 14:30:20 -0500

    Added default searches

---
+++ rest_client/restapi/src/main/java/com/wanderingwombats/cities/restapi/DefaultValues.java
@@ -0,0 +1,17 @@
+package com.wanderingwombats.cities.restapi;
+
+public class DefaultValues {
+	public static final String BASE_URL = "127.0.0.1";
+	
+	public static final String LANDMARKS = "Points of Interest and Landmarks";
+	public static final String ARCHITECTURE = "Architectural Buildings";
+	public static final String RELIGION = "Religious Sites,Churches and Cathedrals";
+	public static final String HISTORY = "Monuments and Statues,Historic Sites,History Museums";
+	public static final String SPORTS = "Arenas and Stadiums";
+	public static final String MUSEUMS = "Museums";
+	public static final String ART = "Art Galleries,Art Museums";
+	public static final String SCIENCE = "Science Museums,Observatories and Planetariums";
+	public static final String NATURE = "Nature and Parks,Nature and Wildlife Areas,Hiking Trails,Biking Trails";
+	public static final String THEATER = "Theaters and Concerts,Theaters,Theater and Performance";
+	public static final String SHOPPING = "Shopping,Shopping Malls,Department Stores";
+}
