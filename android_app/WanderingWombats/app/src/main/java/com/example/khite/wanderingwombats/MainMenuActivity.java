6faece4ce4da39c039ee3e3dd0b6722ddcab8010
Kathryn Hite <Kathryn.Hite@students.olin.edu>
2016-01-23 01:32:04 -0500

    Added diplay map activity and lists paired bluetooth devices

--- android_app/WanderingWombats/app/src/main/java/com/example/khite/wanderingwombats/MainMenuActivity.java
+++ android_app/WanderingWombats/app/src/main/java/com/example/khite/wanderingwombats/MainMenuActivity.java
@@ -1,9 +1,11 @@
 package com.example.khite.wanderingwombats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void openDisplayMapActivity(View view){
+        Intent intent = new Intent(this, DisplayMapActivity.class);
+        startActivity(intent);
     }
 
     @Override
