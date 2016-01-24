f0e776b67c9511014c15d749231768256e5a0ad4
adeaver <deaver.andrew@gmail.com>
2016-01-24 04:14:32 -0500

    Revised REST Client to work with new IP

--- rest_client/restapi/src/main/java/com/wanderingwombats/cities/restapi/DefaultValues.java
+++ rest_client/restapi/src/main/java/com/wanderingwombats/cities/restapi/DefaultValues.java
@@ -1,7 +1,7 @@
 package com.wanderingwombats.cities.restapi;
 
 public class DefaultValues {
-	public static final String BASE_URL = "http://10.0.2.2:5000";
+	public static final String BASE_URL = "http://162.243.13.73";
 	
 	public static final String LANDMARKS = "Sights and Landmarks";
 	public static final String ARCHITECTURE = "Architectural Buildings";
